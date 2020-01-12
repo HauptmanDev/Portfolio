5b8d70b580508c17e47f9993c9226b53c38ec786
HauptmanDev <MainVados@gmail.com>
2020-01-12 10:32:51 +0300

    Remove files

---
+++ build/precache-manifest.8d0dfbb9f7700291e9805d074c4bd489.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace4d633552d3c45b9beffdab24195a5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "aca9bd6576b17dada226",
+    "url": "/Portfolio/static/css/main.ed183287.chunk.css"
+  },
+  {
+    "revision": "7fd894c19c16e5552604",
+    "url": "/Portfolio/static/js/2.0ef97081.chunk.js"
+  },
+  {
+    "revision": "aca9bd6576b17dada226",
+    "url": "/Portfolio/static/js/main.7d00c36b.chunk.js"
+  },
+  {
+    "revision": "e48307059c06d9f98afc",
+    "url": "/Portfolio/static/js/runtime-main.bb0b79a8.js"
+  },
+  {
+    "revision": "2148ffb64cdd8e9495d4171032bae1e5",
+    "url": "/Portfolio/static/media/NeWhtmlLogo.2148ffb6.svg"
+  },
+  {
+    "revision": "6db4332944f59e38ec820138486d8bdb",
+    "url": "/Portfolio/static/media/NewCSSLogo.6db43329.svg"
+  },
+  {
+    "revision": "4f30997f2cae2e86360c6e01039bffcb",
+    "url": "/Portfolio/static/media/NewJSLogo.4f30997f.svg"
+  },
+  {
+    "revision": "aa8bf5ca3e6f41ea81ad52bd7b946ade",
+    "url": "/Portfolio/static/media/NewReactLogo.aa8bf5ca.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/Portfolio/static/media/linkedin-letters.9e36c8fd.svg"
+  },
+  {
+    "revision": "6c2c8e6e46a1c1ee9280dca2283c358d",
+    "url": "/Portfolio/static/media/network.6c2c8e6e.jpg"
+  },
+  {
+    "revision": "b91cc43ec2a72c3e999aff767e2e0c76",
+    "url": "/Portfolio/static/media/telegram.b91cc43e.svg"
+  },
+  {
+    "revision": "d4b26e2e5961b69171b7ef3a03b802ba",
+    "url": "/Portfolio/static/media/vk-social-logotype.d4b26e2e.svg"
+  }
+]);
